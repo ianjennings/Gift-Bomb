7709a65b7983c58e258eb54d4e8c12c0c12305f2
Ian Jennings <ian@meetjennings.com>
2013-02-07 21:53:55 -0500

    shut of lights at nighttime, add extra animations

--- lockbox.ino
+++ lockbox.ino
@@ -7,6 +7,7 @@
 
 int pos = 0;    // variable to store the servo position 
 
+
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
  
@@ -18,21 +19,36 @@ int ledPins[] = {2,3,4,5,6,7,8,9};
 // T1262347200  //noon Jan 1 2010
 
 void setup()  {
+  delay(2000);
   myservo.attach(12);  // attaches the servo on pin 9 to the servo object 
-  myservo.write(90);
+  myservo.write(180);
   Serial.begin(9600);
   for(int i = 0; i <= 7; i++){         //this is a loop and will repeat eight times
     pinMode(ledPins[i],OUTPUT); //we use this to set each LED pin to output
   }     
-  pinMode(11, OUTPUT);
 }
 
-void loop(){       
+void loop(){     
+  
+  for(int i = 0; i < 7; i++){
+    digitalWrite(ledPins[i], LOW);  //Turns on LED #i each time this runs i
+  }        
+  
+  digitalClockDisplay();  
 
   // valentines - todays day
   daysLeft = day(1360872000) - day();
-
-  numToLight = 7 - daysLeft;
+  
+  Serial.println("hour is ");
+  Serial.println(hour());
+  
+  if(hour() > 9 && hour() < 23) {
+    numToLight = 7 - daysLeft;    
+  } else {
+    numToLight = 0;
+    Serial.println("sleep");
+    // delay(60 * 60 * 8);
+  }
   
   Serial.println("days");
   Serial.println(daysLeft);
@@ -40,8 +56,6 @@ void loop(){
   Serial.println("tolight");
   Serial.println(numToLight);
   
-  delay(1000);
-
   for(int i = 0; i < numToLight; i++){
     digitalWrite(ledPins[i], HIGH);  //Turns on LED #i each time this runs i
   }
@@ -54,29 +68,26 @@ void loop(){
       
       for(int i = 0; i < numToLight; i++){
         digitalWrite(ledPins[i], HIGH);  //Turns on LED #i each time this runs i
-        digitalWrite(11, HIGH);  //Turns on LED #i each time this runs i
       }
       delay(80);  
       for(int i = 0; i < numToLight; i++){
         digitalWrite(ledPins[i], LOW);  //Turns on LED #i each time this runs i
-        digitalWrite(11, LOW);  //Turns on LED #i each time this runs i
-
       }   
       delay(100); 
       for(int i = 0; i < numToLight; i++){
         digitalWrite(ledPins[i], HIGH);  //Turns on LED #i each time this runs i
-        digitalWrite(11, HIGH);  //Turns on LED #i each time this runs i
       }
       delay(80);  
       for(int i = 0; i < numToLight; i++){
         digitalWrite(ledPins[i], LOW);  //Turns on LED #i each time this runs i
-        digitalWrite(11, LOW);  //Turns on LED #i each time this runs i
       }   
       delay(3000); 
 
     }
     
   }
+  
+  delay(3000);
 
   if(Serial.available() ) 
   {
@@ -112,3 +123,16 @@ void processSyncMessage() {
   }
 }
 
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
